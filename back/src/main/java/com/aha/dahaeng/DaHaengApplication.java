4c45a9981655615a963c6c33ce556f363a001dec
juheegg <eespacio95@gmail.com>
2021-04-15 10:11:28 +0900

    init

---
+++ back/src/main/java/com/aha/dahaeng/DaHaengApplication.java
@@ -0,0 +1,13 @@
+package com.aha.dahaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaHaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaHaengApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/aha/dahaeng/DaHaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aha.dahaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaHaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
