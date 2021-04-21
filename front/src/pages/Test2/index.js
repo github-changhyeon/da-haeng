cd9bf14e8246fda7fa6ffbce84fd11d4e7fae76e
KIM CHANGHYEON <ckdhyeon95@naver.com>
2021-04-21 10:21:38 +0900

    feat: unity to react send message 테스트 / S04P31B102-57

--- front/src/pages/Test2/index.js
+++ front/src/pages/Test2/index.js
@@ -1,27 +1,58 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 export default function Test2() {
-  const unityContext = new UnityContext({
-    loaderUrl: "UnityBuild2/connection_test.loader.js",
-    dataUrl: "UnityBuild2/connection_test.data",
-    frameworkUrl: "UnityBuild2/connection_test.framework.js",
-    codeUrl: "UnityBuild2/connection_test.wasm",
-  });
+  const [receiveMsg, setReceiveMsg] = useState("데이터 받기 전입니다.");
+  const [unityContext, setUnityContext] = useState(null);
+
+  // const unityContext = new UnityContext({
+  //   loaderUrl: "UnityBuild2/connection_test.loader.js",
+  //   dataUrl: "UnityBuild2/connection_test.data",
+  //   frameworkUrl: "UnityBuild2/connection_test.framework.js",
+  //   codeUrl: "UnityBuild2/connection_test.wasm",
+  // });
+
   const reactUpdateText = () => {
-    unityContext.send("Text1", "ReactUpdateText", "hello react");
+    unityContext.send(
+      "Text1",
+      "ReactUpdateText",
+      "hello react " + new Date().getTime().toString()
+    );
   };
+
+  useEffect(() => {
+    setUnityContext(
+      new UnityContext({
+        loaderUrl: "UnityBuild2/connection_test.loader.js",
+        dataUrl: "UnityBuild2/connection_test.data",
+        frameworkUrl: "UnityBuild2/connection_test.framework.js",
+        codeUrl: "UnityBuild2/connection_test.wasm",
+      })
+    );
+    const unityContext2 = new UnityContext({
+      loaderUrl: "UnityBuild2/connection_test.loader.js",
+      dataUrl: "UnityBuild2/connection_test.data",
+      frameworkUrl: "UnityBuild2/connection_test.framework.js",
+      codeUrl: "UnityBuild2/connection_test.wasm",
+    });
+
+    unityContext2.on("SendTest", (param) => {
+      setReceiveMsg(param);
+    });
+  }, []);
   return (
     <div>
       <h1>hello connection test</h1>
+      <h1>{receiveMsg}</h1>
       <button onClick={() => reactUpdateText()}>Send</button>
-
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Unity
-          style={{ width: "80vw", height: "80vh" }}
-          unityContext={unityContext}
-        />
-      </div>
+      {unityContext ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Unity
+            style={{ width: "80vw", height: "80vh" }}
+            unityContext={unityContext}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
