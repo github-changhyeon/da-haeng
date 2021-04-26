1a87929c9b6076cbac78ff1a73a12ab6662b318d
juheegg <eespacio95@gmail.com>
2021-04-26 14:16:20 +0900

    feat: 회원 기본 구조

--- back/src/main/java/com/aha/dahaeng/user/domain/Admin.java
+++ back/src/main/java/com/aha/dahaeng/user/domain/Admin.java
@@ -3,8 +3,18 @@ package com.aha.dahaeng.user.domain;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * com.aha.dahaeng.user.domain
+ * Admin.java
+ * @date    2021-04-26 오후 2:12
+ * @author  이주희
+ *
+ * @변경이력
+ **/
 
 @Entity
 @Getter
@@ -13,11 +23,15 @@ public class Admin extends User {
 
     private String code;
 
-    public Admin(){}
+    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
+
+    public Admin() {
+    }
 
     @Builder
-    public Admin(Long id, String name, String code) {
-        super(id, name, UserRole.ROLE_ADMIN);
+    public Admin(String loginId, String password, String name, String code) {
+        super(loginId, password, name, UserRole.ROLE_ADMIN);
         this.code = code;
     }
 }
--- back/src/main/java/com/aha/dahaeng/user/repository/UserRepository.java
+++ back/src/main/java/com/aha/dahaeng/user/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package com.aha.dahaeng.user.repository;
 
-public class UserRepository {
+import com.aha.dahaeng.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * com.aha.dahaeng.user.repository
+ * UserRepository.java
+ * @date    2021-04-22 오후 3:09
+ * @author  이주희
+ *
+ * @변경이력
+ **/
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByLoginId(String loginId);
 }
