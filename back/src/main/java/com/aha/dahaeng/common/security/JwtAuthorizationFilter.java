7fd85bbd5342e42b616382b0cbe4f8611e97e258
juheegg <eespacio95@gmail.com>
2021-05-03 17:59:46 +0900

    fix: security 오류 수정

--- back/src/main/java/com/aha/dahaeng/common/security/JwtAuthorizationFilter.java
+++ back/src/main/java/com/aha/dahaeng/common/security/JwtAuthorizationFilter.java
@@ -1,10 +1,16 @@
 package com.aha.dahaeng.common.security;
 
 import com.aha.dahaeng.common.security.jwt.JwtProperties;
+import com.aha.dahaeng.common.security.jwt.JwtProvider;
+import com.aha.dahaeng.user.domain.User;
+import com.aha.dahaeng.user.repository.UserRepository;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -16,26 +22,26 @@ import java.io.IOException;
 /**
  * com.aha.dahaeng.common.security
  * JwtAuthorizationFilter.java
- * @date    2021-04-22 오후 6:09
- * @author  이주희
  *
+ * @author 이주희
+ * @date 2021-04-22 오후 6:09
  * @변경이력
  **/
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-    @Autowired
-    private JwtProperties jwtProperties;
+    private UserRepository userRepository;
 
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository) {
         super(authenticationManager);
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String header = request.getHeader(jwtProperties.getHeader());
+        String header = request.getHeader(JwtProperties.HEADER_STRING);
 
-        if (header == null || !header.startsWith(jwtProperties.getTokenPrefix())) {
+        if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
             chain.doFilter(request, response);
             return;
         }
@@ -48,9 +54,17 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     private Authentication getUsernamePasswordAuthentication(HttpServletRequest request) {
         Authentication authentication = null;
-        String token = request.getHeader(jwtProperties.getHeader());
+        String token = request.getHeader(JwtProperties.HEADER_STRING);
+        if (token != null) {
+            Claims claims = JwtProvider.getClaims(token.replace(JwtProperties.TOKEN_PREFIX, ""));
+            if (claims != null) {
+                User user = userRepository.findByLoginId((String) claims.get("loginId")).orElseThrow(() -> {
+                    return new UsernameNotFoundException("해당 email이 존재하지 않습니다");
+                });
 
-        //TODO token 확인하기
+                authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            }
+        }
 
         return authentication;
     }
--- back/src/main/java/com/aha/dahaeng/common/security/jwt/JwtProvider.java
+++ back/src/main/java/com/aha/dahaeng/common/security/jwt/JwtProvider.java
@@ -2,7 +2,6 @@ package com.aha.dahaeng.common.security.jwt;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.bind.DatatypeConverter;
 import java.util.Date;
@@ -20,42 +19,36 @@ import java.util.Map;
 
 public class JwtProvider {
 
-    @Autowired
-    private JwtProperties jwtProperties;
-
-    public String createAccessToken(JwtDetails principal) {
-        return createToken(principal, jwtProperties.getExpirationTime());
+    public static String createAccessToken(JwtDetails principal) {
+        return createToken(principal, JwtProperties.EXPIRATION_TIME);
     }
 
-    public String createRefreshToken(JwtDetails principal) {
-        return createToken(principal, jwtProperties.getExpirationTime());
+    public static String createRefreshToken(JwtDetails principal) {
+        return createToken(principal, JwtProperties.REFRESH_EXPIRATION_TIME);
     }
 
-    public Map<String, String> createHeader(JwtDetails principal) {
+    public static Map<String, String> createHeader(JwtDetails principal) {
         String token = createAccessToken(principal);
         String refreshToken = createRefreshToken(principal);
 
         Map<String, String> header = new HashMap<String, String>();
-        header.put(jwtProperties.getHeader(), token);
-        header.put(jwtProperties.getRefreshHeader(), refreshToken);
+        header.put(JwtProperties.HEADER_STRING, token);
+        header.put(JwtProperties.REFRESH_HEADER_STRING, refreshToken);
 
         return header;
     }
 
-    public JwtToken createBody(JwtDetails jwtDetails) throws JsonProcessingException {
-        String token = createAccessToken(jwtDetails);
-        String refreshToken = createRefreshToken(jwtDetails);
+    public static JwtToken createBody(JwtDetails jwtDetails) throws JsonProcessingException {
+        String token = JwtProperties.TOKEN_PREFIX + createAccessToken(jwtDetails);
+        String refreshToken = JwtProperties.TOKEN_PREFIX + createRefreshToken(jwtDetails);
 
         return new JwtToken(token, refreshToken);
     }
 
-    public Claims getClaims(String token) {
+    public static Claims getClaims(String token) {
         try {
             Claims claims = parseClaims(token);
-
-
             return claims;
-
         } catch (ExpiredJwtException exception) {
             return null;
         } catch (JwtException exception) {
@@ -65,13 +58,7 @@ public class JwtProvider {
         }
     }
 
-    public String getUsername(String token) throws JwtException {
-        Claims claims = parseClaims(token);
-
-        return (String) claims.get("name");
-    }
-
-    private String createToken(JwtDetails principal, long expirationTime) {
+    private static String createToken(JwtDetails principal, long expirationTime) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + expirationTime);
 
@@ -79,12 +66,12 @@ public class JwtProvider {
                 .setClaims(principal.getClaims())
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+                .signWith(SignatureAlgorithm.HS256, JwtProperties.SECRET)
                 .compact();
     }
 
-    private Claims parseClaims(String token) {
-        return Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(jwtProperties.getSecret()))
+    private static Claims parseClaims(String token) {
+        return Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(JwtProperties.SECRET))
                 .parseClaimsJws(token).getBody();
     }
 
