4f842ecd59e4721f4c4fcc75642dde76824db030
KIM CHANGHYEON <ckdhyeon95@naver.com>
2021-05-10 16:13:24 +0900

    fix : webgl 로딩 추적 / S04P31B102-146
    Former-commit-id: 2e7a39002e843818f41ceee2cb92d4d7597cdb58

--- front/src/pages/Test/index.js
+++ front/src/pages/Test/index.js
@@ -1,22 +1,42 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 export default function Test() {
-  const unityContext = new UnityContext({
-    loaderUrl: "UnityPlaza/UnityPlaza.loader.js",
-    dataUrl: "UnityPlaza/UnityPlaza.data",
-    frameworkUrl: "UnityPlaza/UnityPlaza.framework.js",
-    codeUrl: "UnityPlaza/UnityPlaza.wasm",
-  });
+  const [progress, setProgress] = useState(0.0);
+  const [unityContext, setUnityContext] = useState(null);
+
+  // const unityContext = new UnityContext({
+  //   loaderUrl: "UnityPlaza/UnityPlaza.loader.js",
+  //   dataUrl: "UnityPlaza/UnityPlaza.data",
+  //   frameworkUrl: "UnityPlaza/UnityPlaza.framework.js",
+  //   codeUrl: "UnityPlaza/UnityPlaza.wasm",
+  // });
+
+  useEffect(() => {
+    const temp = new UnityContext({
+      loaderUrl: "UnityPlaza/UnityPlaza.loader.js",
+      dataUrl: "UnityPlaza/UnityPlaza.data",
+      frameworkUrl: "UnityPlaza/UnityPlaza.framework.js",
+      codeUrl: "UnityPlaza/UnityPlaza.wasm",
+    });
+    temp.on("progress", (progression) => {
+      // alert(progression);
+      setProgress(progression);
+    });
+    setUnityContext(temp);
+  }, []);
+
   return (
     <div>
-      <h1>hello webgl Test</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Unity
-          style={{ width: "80vw", height: "80vh" }}
-          unityContext={unityContext}
-        />
-      </div>
+      <h1>로딩률 : {progress * 100}</h1>
+      {unityContext !== null ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Unity
+            style={{ width: "80vw", height: "80vh" }}
+            unityContext={unityContext}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
