ce8ea3cca0dc646532ebb4ffdd0c55afcd64eb9a
김예슬 <loveys1023@naver.com>
2021-05-20 04:14:11 +0900

    hotfix: 빌드 확인 중 / none
    Former-commit-id: de8db9a31ccd3ac7002a56be4cb84fa07da0193a [formerly ee2c95cba6545c247331e44c30d5370920931f04]
    Former-commit-id: a895593bcb57c3e2457f039cc3a7019a44144a69

--- front/src/pages/Test2/index.js
+++ front/src/pages/Test2/index.js
@@ -2,78 +2,37 @@ import { React, useEffect, useState } from 'react';
 import Unity, { UnityContext } from 'react-unity-webgl';
 
 export default function Test2() {
-  const [progress, setProgress] = useState(0.0);
-
-  // const [receiveMsg, setReceiveMsg] = useState("데이터 받기 전입니다.");
-  const [receiveCategory, setReceiveCategory] = useState('');
-  const [receiveStage, setReceiveStage] = useState(0);
-  const [receiveResult, setReceiveResult] = useState('');
+  const [receiveMsg, setReceiveMsg] = useState('데이터 받기 전입니다.');
   const [unityContext, setUnityContext] = useState(null);
-
-  // const unityContext = new UnityContext({
-  //   loaderUrl: "UnityBuild2/connection_test.loader.js",
-  //   dataUrl: "UnityBuild2/connection_test.data",
-  //   frameworkUrl: "UnityBuild2/connection_test.framework.js",
-  //   codeUrl: "UnityBuild2/connection_test.wasm",
-  // });
 
   const reactUpdateText = () => {
     unityContext.send('Text1', 'ReactUpdateText', 'hello react ' + new Date().getTime().toString());
   };
 
-  const sendJsonData = () => {
-    const dataObj = {
-      playerName: '다햄찌',
-      playerID: 888,
-      category: 'burger', // burger bus plaza
-      playType: 'practice', // tutorial exercise practice
-      stage: 1,
-    };
-    // 결과는 string
-    unityContext.send('PlayInfoObj', 'SetPlayInfo', JSON.stringify(dataObj));
-  };
-
-  useEffect(() => {
-    if (progress !== null && progress >= 1) {
-      sendJsonData();
-    }
-  }, [progress]);
-
   useEffect(() => {
     setUnityContext(
       new UnityContext({
-        loaderUrl: 'UnityPlaza/BugerIsPratice3.loader.js',
-        dataUrl: 'UnityPlaza/BugerIsPratice3.data',
-        frameworkUrl: 'UnityPlaza/BugerIsPratice3.framework.js',
-        codeUrl: 'UnityPlaza/BugerIsPratice3.wasm',
+        loaderUrl: 'UnityBuild2/BugerIsPratice3.loader.js',
+        dataUrl: 'UnityBuild2/BugerIsPratice3.data',
+        frameworkUrl: 'UnityBuild2/BugerIsPratice3.framework.js',
+        codeUrl: 'UnityBuild2/BugerIsPratice3.wasm',
       })
     );
     const unityContext2 = new UnityContext({
-      loaderUrl: 'UnityPlaza/BugerIsPratice3.loader.js',
-      dataUrl: 'UnityPlaza/BugerIsPratice3.data',
-      frameworkUrl: 'UnityPlaza/BugerIsPratice3.framework.js',
-      codeUrl: 'UnityPlaza/BugerIsPratice3.wasm',
+      loaderUrl: 'UnityBuild2/BugerIsPratice3.loader.js',
+      dataUrl: 'UnityBuild2/BugerIsPratice3.data',
+      frameworkUrl: 'UnityBuild2/BugerIsPratice3.framework.js',
+      codeUrl: 'UnityBuild2/BugerIsPratice3.wasm',
     });
 
-    unityContext2.on('progress', (progression) => {
-      // alert(progression);
-      setProgress(progression);
-    });
-
-    setUnityContext(unityContext2);
-
-    unityContext2.on('SendResult', (category, stage, result) => {
-      setReceiveCategory(category);
-      setReceiveStage(stage);
-      setReceiveResult(result);
+    unityContext2.on('SendTest', (param) => {
+      setReceiveMsg(param);
     });
   }, []);
   return (
     <div>
       <h1>hello connection test</h1>
-      <h1>{receiveCategory}</h1>
-      <h1>{receiveStage}</h1>
-      <h1>{receiveResult}</h1>
+      <h1>{receiveMsg}</h1>
       <button onClick={() => reactUpdateText()}>Send</button>
       {unityContext ? (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
